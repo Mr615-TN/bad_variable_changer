2251c6d7c84ca4ca3c7fbcfc7912f18993ae1241
Mr615-TN <tneema501@gmail.com>
2024-09-17 20:16:27 -0500

    working on taking in multiple input

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-fn main() {
-    let file = File::open("./*.txt")?;
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        let line = line?;
-        println!("{}", line);
+fn fileLineByLine(file_path: &str) -> io::Result<()> {
+    let file=File::open(file_path)?;
+    let reader=BufReader::new(file);
+   println!("Reading {} line by line: ", filepath);
+   for line in reader.lines() {
+       let line = line?;
+       println!("{}", line);
+   }
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("You must provide at least one file path. ");
+        return Ok(());
+    }
+    for file_path in &args[1..] {
+        fileLineByLine(file_path)?;
     }
     Ok(())
 }
